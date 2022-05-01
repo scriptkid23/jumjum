746457df6ea0f71badd4a069478ba55eaa118f7c
hoando <sieunhannhenkutesn@gmail.com>
2022-05-01 22:17:38 +0700

    feat: update behavior of shell

--- index.js
+++ index.js
@@ -15,17 +15,29 @@ window.onload = function () {
 
     app.loader.add("ju", '/assets/idle.png')
     app.loader.add("shell", '/assets/shell.png')
+    app.loader.add("grass", '/assets/grass/glass.png')
     app.loader.load(doneLoading)
+}
+class Grass extends PIXI.Sprite {
+    constructor(x = 0, y = 0, texture, name = 'none') {
+        super(texture)
+        this.name = name
+        this.x = x;
+        this.y = y;
+        this._zIndex = -1;
+    }
 }
 
 function doneLoading(e) {
     console.log("done")
     // createPlayerSheet();
     // createPlayer();
+    createGrass();
     createTurtleShellSheet();
     // createTurtleShell(200,200);
-    createTurtleShell(100,300);
-    // app.ticker.add(gameLoop)
+    createTurtleShell(130, 300);
+   
+    app.ticker.add(gameLoop)
 }
 
 function createTurtleShellSheet() {
@@ -48,20 +60,26 @@ function createPlayerSheet() {
     for (let i = 1; i <= 25; i++) {
         playerSheet['idle'].push(new PIXI.Texture(ssheet, new PIXI.Rectangle(i * w, 0, w, h)))
     }
-
 }
+
 
 function createTurtleShell(x, y) {
     turtleShell = new PIXI.AnimatedSprite(turtleSheet.active)
-    turtleShell.scale.set(1.5)
+    // turtleShell.scale.set(1.5)
     turtleShell.animationSpeed = 0.15;
     turtleShell.loop = true;
     turtleShell.x = x;
     turtleShell.y = y;
+    turtleShell._zIndex = 5;
+    console.log(turtleShell)
     app.stage.addChild(turtleShell)
     turtleShell.play()
 }
-
+function createGrass() {
+    grass_1 = new Grass(100,327,app.loader.resources['grass'].texture,"Grass1")
+    console.log(grass_1)
+    app.stage.addChild(grass_1)
+}
 function createPlayer() {
     player = new PIXI.AnimatedSprite(playerSheet.idle)
     player.anchor.set(0)
@@ -69,13 +87,14 @@ function createPlayer() {
     app.stage.addChild(player)
     player.play()
 }
-let speed = 2.5
+let speed = 2
+
 function gameLoop(data) {
     turtleShell.x = turtleShell.x + speed;
     console.log(turtleShell.x)
-    if((turtleShell.x > app.view.width - 300) || turtleShell.x < 0){
+    if (turtleShell.x > 149 || turtleShell.x <= 80) {
         speed = -speed;
     }
     console.log("speed:", speed);
-   
+
 }
